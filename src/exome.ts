459e69eb8c2c6cd954673c69d2f29bc783f6b1ea
Marcis Bergmanis <marcisbergmanis@gmail.com>
2021-04-16 23:05:40 +0300

    use `requestAnimationFrame` instead of promise

--- src/exome.ts
+++ src/exome.ts
@@ -15,7 +15,7 @@ export class Exome {
     updateMap[this[exomeId]] = []
 
     // Run this code after constructor to get all the parameters right.
-    Promise.resolve().then(() => {
+    requestAnimationFrame(() => {
       runMiddleware(this, 'NEW', [])
     })
 
