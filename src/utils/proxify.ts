27c6a2b0ebc13a6186d47a8ab3d15ea9b2ca799b
Marcis Bergmanis <marcisbergmanis@gmail.com>
2021-04-19 18:09:36 +0300

    Add IE11 suppoprt

--- src/utils/proxify.ts
+++ src/utils/proxify.ts
@@ -1,7 +1,50 @@
 import { Exome } from '../exome'
 import { runMiddleware } from '../middleware'
 
+function proxifyWithoutPromise<T extends Exome>(parent: T): T {
+  Object.getOwnPropertyNames(Object.getPrototypeOf(parent)).forEach((key) => {
+    const value = (parent as any)[key]
+
+    if (key === 'constructor' || typeof value !== 'function') {
+      return
+    }
+
+    (parent as any)[key] = (...args: any) => {
+      const middleware = runMiddleware(parent, key as any, args)
+
+      const output = value.apply(parent, args)
+
+      if (output instanceof Promise) {
+        return output
+          .then((result) => {
+            if (typeof middleware === 'function') {
+              middleware()
+            }
+
+            return result
+          })
+          .catch((error) => {
+            throw error
+          })
+      }
+
+      if (typeof middleware === 'function') {
+        middleware()
+      }
+
+      return output
+    }
+  })
+
+  return parent
+}
+
 export function proxify<T extends Exome>(parent: T): T {
+  // Handles browsers that don't support Proxy
+  if (!Proxy) {
+    return proxifyWithoutPromise(parent)
+  }
+
   const proxy = new Proxy<T>(
     parent,
     {
@@ -37,19 +80,14 @@ export function proxify<T extends Exome>(parent: T): T {
         }
 
         return value
-      },
-
-      set(target: any, key: string, value) {
-        target[key] = value
-
-        return true
       }
     }
   )
 
-  // It means we're using Proxy polyfill and we should actually allow
-  if (!Object.isExtensible(proxy) && Object.isExtensible(parent)) {
-    return Object.create(proxy)
+  // It means we're using Proxy polyfill, that runs `Object.seal` for target and output objects.
+  // This is not acceptable behavior as it's no longer possible to extend Exome class.
+  if (!Object.isExtensible(proxy)) {
+    throw new Error('Exome class cannot be sealed. This is probably due to invalid Proxy polyfill.')
   }
 
   return proxy
