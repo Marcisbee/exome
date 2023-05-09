39c309844e97f32864fd9ce67d1bcf0be3b833f7
Marcis Bergmanis <marcisbergmanis@gmail.com>
2023-05-09 13:11:12 +0300

    fix lint issues

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const exomeId = Symbol();
 export const exomeName = Symbol();
-export const FUNCTION = 'function';
-export const CONSTRUCTOR = 'constructor';
+export const FUNCTION = "function";
+export const CONSTRUCTOR = "constructor";
--- src/utils/id.ts
+++ src/utils/id.ts
@@ -3,9 +3,9 @@ import { exomeId } from "../constants.ts";
 
 export const getExomeId = (store: Exome): string => {
 	return store[exomeId];
-}
+};
 
 export const setExomeId = (store: Exome, id: string): void => {
 	const [name] = getExomeId(store).split("-");
 	store[exomeId] = `${name}-${id}`;
-}
+};
