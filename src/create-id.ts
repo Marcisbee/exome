064ead2995d7b447fbf86a606448028031703495
Marcis Bergmanis <marcisbergmanis@gmail.com>
2023-11-09 10:34:57 +0200

    increase exome id length

--- src/create-id.ts
+++ src/create-id.ts
@@ -1,4 +1,4 @@
 export const createID = (): string =>
 	(
-		Date.now().toString(36) + ((Math.random() * 1e3) ^ 1).toString(36)
+		Date.now().toString(36) + ((Math.random() * 1e5) ^ 1).toString(36)
 	).toUpperCase();
